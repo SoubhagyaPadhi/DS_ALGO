3274bc6f55c29b3d725adb485de8fd20f3197d7b
User <User@DESKTOP-LQ091A3>
2022-12-11 12:04:45 +0530

    with ssh token git

--- SampleProject/src/SampleProjectMain.java
+++ SampleProject/src/SampleProjectMain.java
@@ -1,6 +1,7 @@
 public class SampleProjectMain {
 	
-	//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
+	//ssh from GIT---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
+	
     public static void main(String[] args) {
         System.out.println("This is test project for ds_algo repo.");
         System.out.println("Second test");
